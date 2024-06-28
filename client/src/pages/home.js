ac33f06d25f9286596e8fe4b80fd784124af3c50
Rahul Gurjar <gurjar.6@iitj.ac.in>
2024-06-29 00:22:55 +0530

    navbar changes

--- client/src/pages/home.js
+++ client/src/pages/home.js
@@ -1,7 +1,20 @@
 export const Home = () => {
     return (
         <div>
-            <h1>This is Home Page</h1>
+            <h1>This is home page</h1>
         </div>
+        // <div>
+        // <img 
+        //     src="/race-image.jpg" 
+        //     alt="Live Race" 
+        //     style={{width: '100%', height: 'auto'}}
+        // />
+        // <h2>live race .</h2>
+        // <div style={{display: 'flex', justifyContent: 'space-around'}}>
+        //     <img src="/user1.jpg" alt="User 1" style={{width: '100px', height: '100px'}} />
+        //     <img src="/user2.jpg" alt="User 2" style={{width: '100px', height: '100px'}} />
+        //     <img src="/user3.jpg" alt="User 3" style={{width: '100px', height: '100px'}} />
+        // </div>
+        // </div>
     );
 }
